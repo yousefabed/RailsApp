a08c3182f605b00d15fc74f7013e64d2a9fd9fc9
YousefAbed <Yousef.Abed@gmail.com>
2012-07-08 18:46:26 -0700

    fixed orders relation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
   attr_accessible :order_date, :order_name, :user_id
   belongs_to :user
-  validates :user_id,  :presence =>true
+  validates :user,  :presence =>true
 end
